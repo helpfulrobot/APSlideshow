d05f0d912ee77b496f4aadf225d5875782bdc2f3
helpfulrobot <chris@silverstripe.com>
2015-12-31 23:59:44 +1300

    Converted to PSR-2

--- code/Slide.php
+++ code/Slide.php
@@ -1,67 +1,69 @@
 <?php
 
-class Slide extends DataObject {
+class Slide extends DataObject
+{
 
-	public static $db = array(
-		"Title" => "Varchar(255)",
-		"SortOrder" => "Int",
-		"GoToURL" => "Text",
-		"Draft" => "Boolean"
-	);
+    public static $db = array(
+        "Title" => "Varchar(255)",
+        "SortOrder" => "Int",
+        "GoToURL" => "Text",
+        "Draft" => "Boolean"
+    );
 
-	public static $has_one = array(
-		"Image" => "Image",
-		"Page" => "Page"
-	);
+    public static $has_one = array(
+        "Image" => "Image",
+        "Page" => "Page"
+    );
 
-	static $default_sort = "SortOrder";
+    public static $default_sort = "SortOrder";
 
-	static $field_labels = array(
-		"Title" => "Caption"
-	);
+    public static $field_labels = array(
+        "Title" => "Caption"
+    );
 
-	static $summary_fields = array(
-		"Thumbnail",
-		"Title",
-		"Draft"
-	);
+    public static $summary_fields = array(
+        "Thumbnail",
+        "Title",
+        "Draft"
+    );
 
-	function getThumbnail() {
-		if (((int) $this->ImageID > 0) && (is_a($this->Image(),'Image')))  {
-			return $this->Image()->SetWidth(50);
-		} else {
-			return "No thumbnails available";
-		}
-	}
+    public function getThumbnail()
+    {
+        if (((int) $this->ImageID > 0) && (is_a($this->Image(), 'Image'))) {
+            return $this->Image()->SetWidth(50);
+        } else {
+            return "No thumbnails available";
+        }
+    }
 
-	public function getCMSFields(){
-		$fields = parent::getCMSFields();
+    public function getCMSFields()
+    {
+        $fields = parent::getCMSFields();
 
-			$fields->removeFieldsFromTab('Root.Main',
-				array(
-					'SortOrder',
-					'Draft',
-					'PageID'
-				)
-			);
+        $fields->removeFieldsFromTab('Root.Main',
+                array(
+                    'SortOrder',
+                    'Draft',
+                    'PageID'
+                )
+            );
 
-			$fields->addFieldToTab("Root.Main", TextField::create("Title")
-				->setTitle("Caption"));
+        $fields->addFieldToTab("Root.Main", TextField::create("Title")
+                ->setTitle("Caption"));
 
-			$fields->addFieldToTab("Root.Main", TextField::create("GoToURL")
-				->setTitle("Go to URL")
-				->setDescription("Start with http://"));
+        $fields->addFieldToTab("Root.Main", TextField::create("GoToURL")
+                ->setTitle("Go to URL")
+                ->setDescription("Start with http://"));
 
-			$fields->addFieldToTab("Root.Main", FieldGroup::create(
-				CheckboxField::create("Draft")->setTitle("Draft?")
-			)->setTitle("Draft"));
+        $fields->addFieldToTab("Root.Main", FieldGroup::create(
+                CheckboxField::create("Draft")->setTitle("Draft?")
+            )->setTitle("Draft"));
 
-			$fields->addFieldToTab("Root.Main", UploadField::create("Image")
-				->setTitle("Image")
-				->setFolderName("APSlideshow")
-				->setConfig("allowedMaxFileNumber", 1));
+        $fields->addFieldToTab("Root.Main", UploadField::create("Image")
+                ->setTitle("Image")
+                ->setFolderName("APSlideshow")
+                ->setConfig("allowedMaxFileNumber", 1));
 
-		return $fields;
-	}
-
+        return $fields;
+    }
 }
--- code/Slideshow.php
+++ code/Slideshow.php
@@ -1,45 +1,47 @@
 <?php
 
-class Slideshow extends DataExtension {
+class Slideshow extends DataExtension
+{
 
-	public static $has_many = array(
-		"Slides" => "Slide"
-	);
+    public static $has_many = array(
+        "Slides" => "Slide"
+    );
 
-	public function updateCMSFields(FieldList $fields) {
-		$config = GridFieldConfig::create();
-		$config->addComponent(new GridFieldToolbarHeader());
-		$config->addComponent(new GridFieldAddNewButton("toolbar-header-right"));
-		$config->addComponent(new GridFieldDataColumns());
-		$config->addComponent(new GridFieldEditButton());
-		$config->addComponent(new GridFieldDeleteAction());
-		$config->addComponent(new GridFieldDetailForm());
-		$config->addComponent(new GridFieldSortableHeader());
-		$config->addComponent(new GridFieldOrderableRows("SortOrder"));
+    public function updateCMSFields(FieldList $fields)
+    {
+        $config = GridFieldConfig::create();
+        $config->addComponent(new GridFieldToolbarHeader());
+        $config->addComponent(new GridFieldAddNewButton("toolbar-header-right"));
+        $config->addComponent(new GridFieldDataColumns());
+        $config->addComponent(new GridFieldEditButton());
+        $config->addComponent(new GridFieldDeleteAction());
+        $config->addComponent(new GridFieldDetailForm());
+        $config->addComponent(new GridFieldSortableHeader());
+        $config->addComponent(new GridFieldOrderableRows("SortOrder"));
 
-		$config->getComponentByType('GridFieldDataColumns')
-			->setFieldCasting(array(
-				"Draft" => "Boolean->Nice"
-			)
-		);
+        $config->getComponentByType('GridFieldDataColumns')
+            ->setFieldCasting(array(
+                "Draft" => "Boolean->Nice"
+            )
+        );
 
-		$gridField = new GridField("Slides", "Slides", $this->owner->Slides(), $config);
-		$fields->addFieldToTab("Root", new Tab("Slideshow", "Slideshow"));
-		$fields->addFieldToTab("Root.Slideshow", $gridField);
-		return $fields;
-	}
-
+        $gridField = new GridField("Slides", "Slides", $this->owner->Slides(), $config);
+        $fields->addFieldToTab("Root", new Tab("Slideshow", "Slideshow"));
+        $fields->addFieldToTab("Root.Slideshow", $gridField);
+        return $fields;
+    }
 }
 
-class Slideshow_Controller extends SiteTreeExtension {
+class Slideshow_Controller extends SiteTreeExtension
+{
 
-	public function contentcontrollerInit($controller) {
-		if ($this->owner->Slides()) {
-			Requirements::javascript(THIRDPARTY_DIR."/jquery/jquery.min.js");
-			Requirements::javascript("APSlideshow/thirdparty/responsiveslides.js");
-			Requirements::javascript("APSlideshow/js/ap.slideshow.js");
-			Requirements::css("APSlideshow/css/APSlideshow.css");
-		}
-
-	}
+    public function contentcontrollerInit($controller)
+    {
+        if ($this->owner->Slides()) {
+            Requirements::javascript(THIRDPARTY_DIR."/jquery/jquery.min.js");
+            Requirements::javascript("APSlideshow/thirdparty/responsiveslides.js");
+            Requirements::javascript("APSlideshow/js/ap.slideshow.js");
+            Requirements::css("APSlideshow/css/APSlideshow.css");
+        }
+    }
 }
